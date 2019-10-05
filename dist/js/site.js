54fcca29798d73bc983b9707404eb1f4ea9ba326
ambientos <ant.grakhov@gmail.com>
2019-10-05 08:10:56 +0500

    Add responsive for header

--- dist/js/site.js
+++ dist/js/site.js
@@ -1,5 +1,17 @@
 (function($){
 	/**
+	 * Mobile menu
+	 */
+	$('.header-nav-list .menu-item-has-children')
+		.on('click', function(e){
+			if ( 'A' != e.target.nodeName && 'a' != e.target.nodeName ) {
+				e.stopPropagation()
+
+				$(this).toggleClass('open')
+			}
+		})
+
+	/**
 	 * Owl Carousel
 	 */
 
